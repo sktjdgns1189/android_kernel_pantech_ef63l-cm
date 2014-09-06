a1b7a92ae83ba5b7a1f7e2d1af3f665eec76a0fe
Say <sktjdgns1189@naver.com>
2014-09-07 00:35:41 +0900

    Fix Vibrator

--- drivers/platform/msm/qpnp-vibrator.c
+++ drivers/platform/msm/qpnp-vibrator.c
@@ -18,6 +18,7 @@
 #include <linux/hrtimer.h>
 #include <linux/of_device.h>
 #include <linux/spmi.h>
+#include <linux/device.h>
 
 #include <linux/qpnp/vibrator.h>
 #include "../../staging/android/timed_output.h"
@@ -35,10 +36,6 @@
 #define QPNP_VIB_VTG_SET_MASK		0x1F
 #define QPNP_VIB_LOGIC_SHIFT		4
 
-static int debug_vib=0;
-
-#define dbg(fmt, args...)   if(debug_vib)printk("[VIB] " fmt, ##args)
-
 struct qpnp_vib {
 	struct spmi_device *spmi;
 	struct hrtimer vib_timer;
@@ -53,22 +50,50 @@ struct qpnp_vib {
 	int timeout;
 	struct mutex lock;
 };
-#ifdef VIBRATOR_PANTECH_PATCH
-void pantech_vib_debug_enable(void);
-void pantech_vib_debug_disable(void);
-void pantech_vib_debug_enable(void){
-  printk("[VIB] Debug_vib enable is called\n");
-  debug_vib=1;
-  return;
-}
-void pantech_vib_debug_disable(void){
-  printk("[VIB] Debug_vib disable is called\n");
-  debug_vib=0;
-  return;
-}
-#endif
 
 static struct qpnp_vib *vib_dev;
+
+static ssize_t qpnp_vib_level_show(struct device *dev,
+					struct device_attribute *attr,
+					char *buf)
+{
+	struct timed_output_dev *tdev = dev_get_drvdata(dev);
+	struct qpnp_vib *vib = container_of(tdev, struct qpnp_vib,
+					 timed_dev);
+
+	return scnprintf(buf, PAGE_SIZE, "%d\n", vib->vtg_level);
+}
+
+static ssize_t qpnp_vib_level_store(struct device *dev,
+					struct device_attribute *attr,
+					const char *buf, size_t count)
+{
+	struct timed_output_dev *tdev = dev_get_drvdata(dev);
+	struct qpnp_vib *vib = container_of(tdev, struct qpnp_vib,
+					 timed_dev);
+	int val;
+	int rc;
+
+	rc = kstrtoint(buf, 10, &val);
+	if (rc) {
+		pr_err("%s: error getting level\n", __func__);
+		return -EINVAL;
+	}
+
+	if (val < QPNP_VIB_MIN_LEVEL) {
+		pr_err("%s: level %d not in range (%d - %d), using min.", __func__, val, QPNP_VIB_MIN_LEVEL, QPNP_VIB_MAX_LEVEL);
+		val = QPNP_VIB_MIN_LEVEL;
+	} else if (val > QPNP_VIB_MAX_LEVEL) {
+		pr_err("%s: level %d not in range (%d - %d), using max.", __func__, val, QPNP_VIB_MIN_LEVEL, QPNP_VIB_MAX_LEVEL);
+		val = QPNP_VIB_MAX_LEVEL;
+	}
+
+	vib->vtg_level = val;
+
+	return strnlen(buf, count);
+}
+
+static DEVICE_ATTR(vtg_level, S_IRUGO | S_IWUSR, qpnp_vib_level_show, qpnp_vib_level_store);
 
 static int qpnp_vib_read_u8(struct qpnp_vib *vib, u8 *data, u16 reg)
 {
@@ -147,35 +172,9 @@ static int qpnp_vib_set(struct qpnp_vib *vib, int on)
 {
 	int rc;
 	u8 val;
-#ifdef VIBRATOR_PANTECH_PATCH  
+
 	if (on) {
 		val = vib->reg_vtg_ctl;
-		dbg("[VIB] ON -> %d, vib->reg_vtg_ctl -> %d, vib->vtg_level -> %d \n",on,val,vib->vtg_level);
-		val &= ~QPNP_VIB_VTG_SET_MASK;
-		val |= (vib->vtg_level & QPNP_VIB_VTG_SET_MASK);
-		val=on;
-		rc = qpnp_vib_write_u8(vib, &val, QPNP_VIB_VTG_CTL(vib->base));
-		if (rc < 0)
-			return rc;
-		vib->reg_vtg_ctl = val;
-		val = vib->reg_en_ctl;
-		val |= QPNP_VIB_EN;
-		rc = qpnp_vib_write_u8(vib, &val, QPNP_VIB_EN_CTL(vib->base));
-		if (rc < 0)
-			return rc;
-		vib->reg_en_ctl = val;
-	} else {
-		val = vib->reg_en_ctl;
-		val &= ~QPNP_VIB_EN;
-		dbg("[VIB] OFF -> %d, vib->reg_vtg_ctl -> %d \n",on,val);
-		rc = qpnp_vib_write_u8(vib, &val, QPNP_VIB_EN_CTL(vib->base));
-		if (rc < 0)
-			return rc;
-		vib->reg_en_ctl = val;
-	}
-#else
-if (on) {
-		val = vib->reg_vtg_ctl;
 		val &= ~QPNP_VIB_VTG_SET_MASK;
 		val |= (vib->vtg_level & QPNP_VIB_VTG_SET_MASK);
 		rc = qpnp_vib_write_u8(vib, &val, QPNP_VIB_VTG_CTL(vib->base));
@@ -196,8 +195,6 @@ if (on) {
 			return rc;
 		vib->reg_en_ctl = val;
 	}
-
-#endif
 
 	return rc;
 }
@@ -206,29 +203,10 @@ static void qpnp_vib_enable(struct timed_output_dev *dev, int value)
 {
 	struct qpnp_vib *vib = container_of(dev, struct qpnp_vib,
 					 timed_dev);
-#ifdef VIBRATOR_PANTECH_PATCH
-  int vib_strength = ((value & 0xFFFF0000) >> 16) ;
-  int vib_timeout = (value & 0x0000FFFF);
-  dbg("[VIB] vib_strength ->%d, vib_timeout -> %d\n",vib_strength,vib_timeout);
-#endif
 
 	mutex_lock(&vib->lock);
 	hrtimer_cancel(&vib->vib_timer);
-#ifdef VIBRATOR_PANTECH_PATCH
-  if (vib_strength == 0)
-		vib->state = 0;
-	else {
-		vib_timeout = (vib_timeout > vib->timeout ?
-				 0x7FFFFFFF : vib_timeout);
-	  if(vib_strength > 31) vib_strength = 31;
-		//vib->state = ((vib_strength*70)/100) + 6 + 11;
-		vib->state = vib_strength;
-		hrtimer_start(&vib->vib_timer,
-			      ktime_set(vib_timeout / 1000, (vib_timeout % 1000) * 1000000),
-			      HRTIMER_MODE_REL);
-	}
 
-#else
 	if (value == 0)
 		vib->state = 0;
 	else {
@@ -239,13 +217,8 @@ static void qpnp_vib_enable(struct timed_output_dev *dev, int value)
 			      ktime_set(value / 1000, (value % 1000) * 1000000),
 			      HRTIMER_MODE_REL);
 	}
-#endif
 	mutex_unlock(&vib->lock);
-#ifdef VIBRATOR_PANTECH_PATCH
-  qpnp_vib_set(vib, vib->state);
-#else
 	schedule_work(&vib->work);
-#endif
 }
 
 static void qpnp_vib_update(struct work_struct *work)
@@ -363,6 +336,8 @@ static int __devinit qpnp_vibrator_probe(struct spmi_device *spmi)
 	rc = timed_output_dev_register(&vib->timed_dev);
 	if (rc < 0)
 		return rc;
+		
+	device_create_file(vib->timed_dev.dev, &dev_attr_vtg_level);
 
 	vib_dev = vib;
 
