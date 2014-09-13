fbfa0fb90ac245517b14c7ef1aa9775e819f4980
Say <sktjdgns1189@naver.com>
2014-09-13 12:53:17 +0900

    misc: modify build scripts

--- clean_kernel.sh
+++ clean_kernel.sh
@@ -1,26 +1,2 @@
 #!/bin/bash
-###############################################################################
-#
-#                           Clean Build Script
-#
-###############################################################################
-# 2013-01-14    P12104  :   created
-###############################################################################
-make O=./obj/KERNEL_OBJ/ clean
-
-make O=./obj/KERNEL_OBJ/ clean
-if [ -f ./mkbootimg/boot.img ]
-then
-    rm ./mkbootimg/boot.img
-fi
-
-if [ -f ./mkbootimg/zImage ]
-then
-    rm ./mkbootimg/zImage
-fi
-
-if [ -d ./obj/ ]
-then
-    rm -r ./obj/
-fi
- 
+make mrproper
